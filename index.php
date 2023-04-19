f165e4b84033936668006681d2f7dae7bd0b5cff
NikhilTM3 <113019048+NikhilTM3@users.noreply.github.com>
2023-04-19 19:15:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hi welcome to php-deploy in declarative method </p>'
+ <?php echo '<p>Hi welcome to php-deploy in declarative method !!!! </p>'
  </body>
 </html>
